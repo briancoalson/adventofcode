d8eae88ba846614768604ddfebdddba3c4f9d801
Brian Coalson <brian.coalson@asynchrony.com>
2018-12-04 16:49:30 -0600

    After a little Googling, looks like awk is the simplest way of doing this.

--- day1/day1.sh
+++ day1/day1.sh
@@ -1,10 +1,12 @@
-nums=$(cat $1)
+#!/bin/bash
+awk '{ sum += $1 } END { print sum }' $1
 
-answer=0
 
-for num in $nums
-do
-	answer=$(($answer + $num))
-done
 
-echo $answer
+
+
+#answer=0
+#while read num || [[ -n $num ]]; do
+#	answer=$(($answer + $num))
+#done < $1
+#echo $answer

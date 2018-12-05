d476bcdb980e65bf1401dfdef101afe119e6aa0c
Brian Coalson <brian.coalson@asynchrony.com>
2018-12-04 22:23:48 -0600

    Adding formatting for the output.

--- day1/day1.sh
+++ day1/day1.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 #Part 1
+echo "Sum of frequencies:"
 awk '{ sum += $1 } END { print sum }' $1
 
 #Part 2
+echo "First repeated frequency:"
 frequencies=$(cat $1)
 
 sum=0
@@ -13,7 +15,7 @@ while [[ "$twice" != true ]]; do
 	for freq in $frequencies
 	do :
 		sum=$(($sum + $freq))
-		
+
 		if [[ -n "${sums[$sum]}" ]]; then
 			echo $sum
 			twice=true

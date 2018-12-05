050703d1f82e8ef5c8652edc43bbea1c79e2ed9b
Brian Coalson <brian.coalson@asynchrony.com>
2018-12-05 12:36:50 -0600

    Working, but slow solution, requires manual inspection to get characters in common.

--- day2/day2.sh
+++ day2/day2.sh
@@ -3,15 +3,40 @@
 #part 1
 boxIDs=$(cat input.txt)
 
-twice=0
-thrice=0
+# twice=0
+# thrice=0
 
-for id in $boxIDs; do
-	tmp=$(echo $id | fold -w1 | sort | uniq -c)
-	twice=$(($twice + $(echo $tmp | grep 2 | wc -l)))
-	thrice=$(($thrice + $(echo $tmp | grep 3 | wc -l)))
-done
+# for id in $boxIDs; do
+# 	tmp=$(echo $id | fold -w1 | sort | uniq -c)
+# 	twice=$(($twice + $(echo $tmp | grep 2 | wc -l)))
+# 	thrice=$(($thrice + $(echo $tmp | grep 3 | wc -l)))
+# done
 
-echo $(($twice * $thrice))
+# echo $(($twice * $thrice))
+
 
 #part 2
+#assuming same length strings, probably would want error handling
+character_difference() {
+
+	diff=0
+
+	for (( i = 0; i < ${#1}; i++ )); do
+		if [[ ${1:$i:1} != ${2:$i:1} ]]; then
+			diff=$(($diff + 1))
+		fi
+	done
+
+	echo $diff
+}
+
+for id in $boxIDs; do
+	for id2 in $boxIDs; do
+		if [[ $(character_difference $id $id2) -eq 1 ]]; then
+			echo "Match!"
+			echo $id
+			echo $id2
+			break
+		fi
+	done
+done

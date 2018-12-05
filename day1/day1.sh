54ba370c393eb0324b1213f6066cb5247b4255d9
Brian Coalson <brian.coalson@asynchrony.com>
2018-12-04 19:00:11 -0600

    I have something that will iterate through the frequencies and check if there is a repeat.

--- day1/day1.sh
+++ day1/day1.sh
@@ -1,12 +1,44 @@
 #!/bin/bash
 awk '{ sum += $1 } END { print sum }' $1
 
+# read -d '/n' -a frequencies < $1
+# this is fucked, but I don't know why, seems better than cat into an array
+# echo ${#frequencies[@]}
+
+# num_frequencies=$(cat $1 | wc -l)
+# echo $num_frequencies
+
+# twice=$FALSE
+# # while !($twice); do
+# 	for freq in $frequencies; 
+# 	do
+# 		echo $freq
+# 	done
+# # done
 
 
+frequencies=$(cat $1)
 
+declare -a sums
 
-#answer=0
-#while read num || [[ -n $num ]]; do
-#	answer=$(($answer + $num))
-#done < $1
-#echo $answer
+sum=0
+
+for freq in $frequencies
+do :
+	sum=$(($sum + $freq))
+	echo $sum
+
+#WTF?!  Why does $frequencies work but not $sums?
+	for i in "${sums[@]}"
+	do :
+		if [[ $i -eq $sum ]]
+		then
+			echo "help!"
+		fi
+	done
+
+	sums=("${sums[@]}" "$sum")
+
+done
+
+echo ${#sums[@]}
